e9374def5569eeba72703753af8f4f0c18967a4d
mt-torres <torres@LAPTOP-205G42KP.localdomain>
2022-01-09 18:01:28 -0300

    Add higLigthjs and option to select language

---
+++ src/js/highlight.js
@@ -0,0 +1,19 @@
+const codeArea = document.querySelector("[data-codigo=code-area]");
+const codeEditor = document.querySelector("[data-codigo=code]");
+const btnHl = document.querySelector("[ data-codigo=button]");
+
+codeArea.addEventListener('click', ()=> {
+    codeEditor.focus();
+    console.log(codeEditor)
+})
+
+
+btnHl.addEventListener('click',(evento)=> {
+    const language = evento.target.closest("[data-projeto=main]").querySelector("[data-dados=linguagem]").value;
+    const codigo = codeArea.innerText;
+    codeArea.innerHTML = `<code class="projeto-codigo__editor hljs language-${language}"  data-codigo="code" spellcheck="false" contenteditable="true"></code>`;
+    codeArea.querySelector('code').textContent = codigo
+    hljs.highlightElement(codeArea.querySelector('code'))
+})
+
+
